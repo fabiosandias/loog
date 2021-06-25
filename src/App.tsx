988e6330634bfb8b751fa5d460f074a08f4dc84d
Fabio Dias <fabiowebmain@gmail.com>
2021-06-25 08:22:21 -0300

    Instalação das libs: Redux, react-redux, react-router-dom e styles-components. Estrutura de thema com styles-components. Inclusão da logo da Loog com base64. Limpeza de codigo no geral

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { logoImageBase64 } from "utils/constants";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "styles/themes/default.theme";
 
-function App() {
-  return (
+const App = () => (
+  <ThemeProvider theme={defaultTheme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logoImageBase64} alt="" />
     </div>
-  );
-}
+  </ThemeProvider>
+);
 
 export default App;
